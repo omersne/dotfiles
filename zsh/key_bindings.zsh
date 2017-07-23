aa66c436b8c54998edea004bbe9e01eb5f21e8e3
Omer Sne <omer@omersne.com>
2017-07-23 18:07:24 +0300

    Improve key bindings

--- zsh/key_bindings.zsh
+++ zsh/key_bindings.zsh
@@ -1,28 +1,9 @@
-
-bindkey "^[^[[C" forward-word # alt + right arrow
-bindkey "^[^[[D" backward-word # alt + left arrow
-
-bindkey "^[[1;10C" end-of-line # alt + shift + right arrow
-bindkey "^[^[[B" end-of-line # alt + down arrow
-
-bindkey "^[[1;10D" beginning-of-line # alt + shift + left arrow
-bindkey "^[^[[A" beginning-of-line # alt + up arrow
-
-bindkey "^[[1;2C" delete-char # shift + right_arrow
-bindkey "^[[1;2D" backward-delete-char # shift + left_arrow
-
-bindkey "^[[F" delete-char # shift + right_arrow
-bindkey "^[[H" backward-delete-char # shift + left_arrow
-
 _delete_to_end_of_line()
 {
     BUFFER="${BUFFER:0:$CURSOR}"
     CURSOR="${#BUFFER}"
 }
 zle -N _delete_to_end_of_line
-bindkey '^[[6~' _delete_to_end_of_line # fn + down arrow
-bindkey '^[[1;2B' _delete_to_end_of_line # shift + down arrow
-
 
 _delete_to_start_of_line()
 {
@@ -30,9 +11,6 @@ _delete_to_start_of_line()
     CURSOR="0"
 }
 zle -N _delete_to_start_of_line
-bindkey '^[[5~' _delete_to_end_of_line # fn + down arrow
-bindkey '^[[1;2A' _delete_to_start_of_line # shift + up arrow
-
 
 _while_true_do()
 {
@@ -40,7 +18,6 @@ _while_true_do()
     CURSOR="15"
 }
 zle -N _while_true_do
-bindkey '^[OP' _while_true_do # fn + F1
 
 _while_i_is_smaller_than()
 {
@@ -48,7 +25,6 @@ _while_i_is_smaller_than()
     CURSOR="20"
 }
 zle -N _while_i_is_smaller_than
-bindkey '^[[1;2P' _while_i_is_smaller_than # fn + shift + F1
 
 _for_i_in_do()
 {
@@ -56,7 +32,6 @@ _for_i_in_do()
     CURSOR="9"
 }
 zle -N _for_i_in_do
-bindkey '^[OQ' _for_i_in_do # fn + F2
 
 _c_style_for_loop()
 {
@@ -64,7 +39,6 @@ _c_style_for_loop()
     CURSOR="15"
 }
 zle -N _c_style_for_loop
-bindkey '^[[1;2Q' _c_style_for_loop # fn + shift + F2
 
 _awk_space_divider()
 {
@@ -72,7 +46,6 @@ _awk_space_divider()
     CURSOR="$(( ${#BUFFER} - 2 ))"
 }
 zle -N _awk_space_divider
-bindkey '^[OR' _awk_space_divider # fn + F3
 
 _tr_newline_to_space()
 {
@@ -80,7 +53,6 @@ _tr_newline_to_space()
     CURSOR="${#BUFFER}"
 }
 zle -N _tr_newline_to_space
-bindkey '^[OS' _tr_newline_to_space # fn + F4
 
 _tr_space_to_newline()
 {
@@ -88,6 +60,66 @@ _tr_space_to_newline()
     CURSOR="${#BUFFER}"
 }
 zle -N _tr_space_to_newline
-bindkey '^[[1;2S' _tr_space_to_newline # fn + shift + F4
 
+typeset -A ___key_bindings
+if ___is_tmux_session; then
+    ___key_bindings=(
+        "^[[1;3C" forward-word # alt + right arrow
+        "^[[1;3D" backward-word # alt + left arrow
+
+        "^[[1;4C" end-of-line # alt + shift + right arrow
+        "^[[1;4B" end-of-line # alt + down arrow
+
+        "^[[1;4D" beginning-of-line # alt + shift + left arrow
+        "^[[1;4A" beginning-of-line # alt + up arrow
+
+        "^[[1;2C" delete-char # shift + right_arrow
+        "^[[1;2D" backward-delete-char # shift + left_arrow
+
+        "^[[1;2A" _delete_to_end_of_line # shift + down arrow
+        "^[[1;2A" _delete_to_start_of_line # shift + up arrow
+    )
+else
+    ___key_bindings=(
+        "^[^[[C" forward-word # alt + right arrow
+        "^[^[[D" backward-word # alt + left arrow
+
+        "^[[1;10C" end-of-line # alt + shift + right arrow
+        "^[^[[B" end-of-line # alt + down arrow
+
+        "^[[1;10D" beginning-of-line # alt + shift + left arrow
+        "^[^[[A" beginning-of-line # alt + up arrow
+
+        "^[[1;2C" delete-char # shift + right_arrow
+        "^[[1;2D" backward-delete-char # shift + left_arrow
+
+        "^[[F" delete-char # shift + right_arrow
+        "^[[H" backward-delete-char # shift + left_arrow
+
+        "^[[6~" _delete_to_end_of_line # fn + down arrow
+        "^[[1;2B" _delete_to_end_of_line # shift + down arrow
+
+        "^[[5~" _delete_to_end_of_line # fn + down arrow
+        "^[[1;2A" _delete_to_start_of_line # shift + up arrow
+
+        #"^[[6~" _delete_to_end_of_line # fn + down arrow
+        "^[[1;2B" _delete_to_end_of_line # shift + down arrow
+        #"^[[5~" _delete_to_end_of_line # fn + down arrow
+        "^[[1;2A" _delete_to_start_of_line # shift + up arrow
+
+        # TODO: tmux support
+        "^[OP" _while_true_do # fn + F1
+        "^[[1;2P" _while_i_is_smaller_than # fn + shift + F1
+        "^[OQ" _for_i_in_do # fn + F2
+        "^[[1;2Q" _c_style_for_loop # fn + shift + F2
+        "^[OR" _awk_space_divider # fn + F3
+        "^[OS" _tr_newline_to_space # fn + F4
+        "^[[1;2S" _tr_space_to_newline # fn + shift + F4
+    )
+fi
+
+for key in "${(@k)___key_bindings}"; do
+    bindkey "$key" "${___key_bindings[$key]}"
+done
+unset key
 
