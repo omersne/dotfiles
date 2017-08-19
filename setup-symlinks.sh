671bee57864d509d118f1cba19112ab1c15cb9f9
Omer Sne <omer@omersne.com>
2017-08-19 15:34:16 +0300

    Improve setup-symlinks.sh.

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -2,80 +2,123 @@
 
 DOTFILES_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . $DOTFILES_DIR/.colors
+. $DOTFILES_DIR/.functions
 
-echo "$DOTFILES_DIR" > $HOME/.dotfiles_dir_path
+NO_REPLACE=0
 
-[ -n "$GIT_DIR" ] && echo "$GIT_DIR" > ~/.git_dir_path
+abort()
+{
+    local message="$1"
 
-for symlink in $(find $DOTFILES_DIR -maxdepth 1 -type f -name ".*"); do
+    echo "$message" >&2
+    exit 1
+}
 
-    filename="${symlink##*/}"
-    replaced_by_symlinks_dir=$HOME/.replaced_by_symlinks/
+symlink_file_and_keep_old()
+{
+    local src="$1"
+    local dst="$2"
 
-    # If it's an example file
-    if [ "${filename%.example}" != "${filename}" ]; then
-
-        if [ -e $HOME/${filename%.example} ]; then
-            continue
-        else
-            # Copy the file, so a symlink won't be created. The file will be 
-            # renamed to ${filename%.example} later and will be edited with the 
-            # local config.
-            cp $filename $HOME/
-            continue
-        fi
+    if [ -d "$dst" ]; then
+        dst="${dst%/}/${src##*/}"
     fi
 
-    if [ -h $HOME/$filename ]; then
+    # XXX: [ -h ] is needed for symlinks that
+    # point to files that don't exist.
+    if [ -e "$dst" ] || [ -h "$dst" ]; then
+        local the_date="$(date_for_filename)"
+        echo_info "A file named '$dst' already exists."
+        if [ "$NO_REPLACE" == 1 ]; then
+            return 0
+        fi
 
-        rm $HOME/$filename && ln -s $symlink $HOME/$filename
-
-    elif [ -f $HOME/$filename ]; then
-
-        the_date=$(date +%Y-%m-%d_%H-%M)
-
-        echo_info "A file named $filename already exists in $HOME."
-        echo_info "It will be renamed to $filename.old_$the_date"
-
-        if mv $HOME/$filename $HOME/$filename.old_$the_date; then
-            ln -s $symlink $HOME/$filename
-        else
+        echo_info "It will be renamed to '$dst.old_$the_date'."
+        mv "$dst" "$dst.old_$the_date"
+        if [ $? -ne 0 ]; then
             echo_error "Failed to rename $filename"
+            ((ERRORS++))
+            return 1
+        fi
+    fi
+
+    ln -s "$src" "$dst"
+}
+
+symlink_general_dotfiles()
+{
+    local symlink filename
+    for symlink in $(find $DOTFILES_DIR -maxdepth 1 -type f -name ".*"); do
+        filename="${symlink##*/}"
+
+        if endswith "$filename" ".example"; then
+            if [ -e $HOME/${filename%.example} ]; then
+                continue
+            else
+                # Copy the file, so a symlink won't be created. The file will be
+                # renamed to ${filename%.example} later and will be edited with the
+                # local config.
+                cp $filename $HOME/
+                continue
+            fi
         fi
 
+        symlink_file_and_keep_old "$symlink" "$HOME/"
+    done
+}
+
+symlink_vim_files()
+{
+    [ ! -d $HOME/.vim/colors ] && mkdir -p $HOME/.vim/colors
+
+    local symlink
+    for symlink in $(find $DOTFILES_DIR/vim/color_schemes/ -maxdepth 1 -type f); do
+        symlink_file_and_keep_old "$symlink" "$HOME/.vim/colors/"
+    done
+}
+
+symlink_gpg_files()
+{
+    [ ! -d $HOME/.gnupg ] && mkdir -p $HOME/.gnupg
+
+    local symlink
+    for symlink in $(find $DOTFILES_DIR/.gnupg/ -maxdepth 1 -type f); do
+        symlink_file_and_keep_old "$symlink" "$HOME/.gnupg/"
+    done
+}
+
+run_func()
+{
+    local func_name="$1"
+    (
+    ERRORS=0
+    $func_name
+    if [ $ERRORS -eq 0 ]; then
+        exit 0
     else
-
-        ln -s $symlink $HOME/$filename
+        exit 1
     fi
-done
+    )
+}
 
-# Vim color schemes
-mkdir -p $HOME/.vim/colors
+main()
+{
+    local arg
+    for arg in "$@"; do
+        case "$arg" in
+            --no-replace|-n) NO_REPLACE=1;;
+            *) abort "Invalid argument: \`$arg'";;
+        esac
+    done
 
-for symlink in $(find $DOTFILES_DIR/vim/color_schemes/ -maxdepth 1 -type f); do
+    local rc=0
 
-    filename="${symlink##*/}"
+    echo "$DOTFILES_DIR" > $HOME/.dotfiles_dir_path
+    [ -n "$GIT_DIR" ] && echo "$GIT_DIR" > ~/.git_dir_path
 
-    if [ -h $HOME/.vim/colors/$filename ]; then
+    #run_func symlink_general_dotfiles || rc=1
+    #run_func symlink_vim_files || rc=1
+    run_func symlink_gpg_files || rc=1
 
-        rm $HOME/.vim/colors/$filename && ln -s $symlink $HOME/.vim/colors/
-
-    elif [ -f $HOME/.vim/colors/$filename ]; then
-
-        the_date=$(date +%Y-%m-%d_%H-%M)
-
-        echo_info "A file named $filename already exists in $HOME/.vim/colors/."
-        echo_info "It will be renamed to $filename.old_$the_date"
-
-        mv $HOME/.vim/colors/$filename $HOME/.vim/colors/$filename.old_$the_date
-
-        ln -s $symlink $HOME/.vim/colors/
-
-    else
-
-        ln -s $symlink $HOME/.vim/colors/
-
-    fi
-
-done
-
+    exit $rc
+}
+main "$@"
