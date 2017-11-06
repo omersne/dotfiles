06c6352edd4bb4ee7310f41666447f670a818bc2
Omer Sne <omer@omersne.com>
2017-11-06 09:46:54 +0200

    Add an option for parsing JSON files as OrderedDicts.

--- bin/make-json-readable.py
+++ bin/make-json-readable.py
@@ -3,19 +3,22 @@
 import argparse
 import json
 import sys
+from collections import OrderedDict
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", nargs="?", default=sys.stdin)
+    parser.add_argument("--ordereddict", "-o", dest="object_pairs_hook",
+                        action="store_const", const=OrderedDict, default=None,
+                        help="Parse the JSON file as an OrderedDict.")
+    parser.add_argument("json_file", nargs="?", default=sys.stdin,
+                        type=lambda f: open(f, "r"))
     args = parser.parse_args()
 
-    if hasattr(args.filename, "read"):
-        j = json.load(args.filename)
-    else:
-        with open(args.filename, "r") as f:
-            j = json.load(f)
+    j = json.load(args.json_file, object_pairs_hook=args.object_pairs_hook)
+    if args.json_file != sys.stdin:
+        args.json_file.close()
 
-    print(json.dumps(j, indent=4, sort_keys=True))
+    print(json.dumps(j, indent=4, sort_keys=args.object_pairs_hook is None))
 
     sys.exit(0)
 
