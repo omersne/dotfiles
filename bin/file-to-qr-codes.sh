4d13fdeff6230e8b38aa0a464ffaa2d27d7496f9
Omer Sne <omer@omersne.com>
2019-05-11 18:25:50 +0300

    Support different error correction levels.

--- bin/file-to-qr-codes.sh
+++ bin/file-to-qr-codes.sh
@@ -12,13 +12,23 @@
 #
 # :authors: Omer Sne, @omersne, 0x65A9D22B299BA9B5
 # :date: 2018-10-07
-# :version: 0.0.1
+# :version: 0.0.2
 ##############################################################################
 
 set -e
 
-# https://github.com/fukuchi/libqrencode/issues/31#issuecomment-120911314
-MAX_CHUNK_LENGTH=2953
+ERROR_CORRECTION_LEVEL="${ERROR_CORRECTION_LEVEL:-H}"
+case "$ERROR_CORRECTION_LEVEL" in
+    # https://github.com/fukuchi/libqrencode/issues/31#issuecomment-120911314
+    L) MAX_CHUNK_LENGTH=2953;;
+    M) MAX_CHUNK_LENGTH=2331;;
+    Q) MAX_CHUNK_LENGTH=1663;;
+    H) MAX_CHUNK_LENGTH=1273;;
+    *)
+        echo "Invalid error correction level: '$ERROR_CORRECTION_LEVEL'" >&1
+        exit 1
+        ;;
+esac
 
 SPLIT="split"
 # The default version of `split' on Mac OS is problematic.
@@ -42,6 +52,6 @@ else
 fi
 
 for file in "${FILES_TO_CONVERT[@]}"; do
-    qrencode -8 -o "${file}_qr_code.png" < "$file"
-    qrencode -8 -t SVG -o "${file}_qr_code.svg" < "$file"
+    qrencode -8 -l "$ERROR_CORRECTION_LEVEL" -o "${file}_qr_code.png" < "$file"
+    qrencode -8 -l "$ERROR_CORRECTION_LEVEL" -t SVG -o "${file}_qr_code.svg" < "$file"
 done
