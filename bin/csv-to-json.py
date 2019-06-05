a27ab4a52b493f594ebee71fde024242e7258d50
Omer Sne <omer@omersne.com>
2019-06-05 20:38:57 +0300

    Improve file I/O, make pylint happy.

--- bin/csv-to-json.py
+++ bin/csv-to-json.py
@@ -10,7 +10,7 @@
 #
 # :authors: Omer Sne, @omersne, 0x65A9D22B299BA9B5
 # :date: 2018-07-02
-# :version: 0.0.1
+# :version: 0.0.2
 ##############################################################################
 
 import argparse
@@ -20,12 +20,16 @@ import sys
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("csv_file", nargs="?", default=sys.stdin,
-                        type=lambda f: open(f, "r"))
+    parser.add_argument("csv_file", nargs="?", default=sys.stdin)
     args = parser.parse_args()
 
-    dr = csv.DictReader(args.csv_file)
-    j = [entry for entry in dr]
+    if hasattr(args.csv_file, "read"):
+        reader = csv.DictReader(args.csv_file)
+        j = [entry for entry in reader]
+    else:
+        with open(args.csv_file, "r") as file_obj:
+            reader = csv.DictReader(file_obj)
+            j = [entry for entry in reader]
 
     print(json.dumps(j, indent=4, sort_keys=True))
     sys.exit(0)
